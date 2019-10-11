9dc846fede2571ba6917a839983588c4538f7881
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2019-10-11 14:55:03 -0300

    create ui for app

--- alcool_gasolina/lib/main.dart
+++ alcool_gasolina/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:alcool_gasolina/Home.dart';
 import 'package:flutter/material.dart';
 
 void main(){
  runApp(MaterialApp(
    debugShowCheckedModeBanner: false,
-   home: ,
+   home: Home(),
  ));
 }
 
